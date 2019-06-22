edc03b186055c6074f4a4575aa4f5b70fdd29ad7
Coreplo <coreplo@gmail.com>
2019-06-22 15:30:15 +0200

    testting some polymorphism in c#

--- Program.cs
+++ Program.cs
@@ -10,12 +10,54 @@ namespace gitVS2017
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+
+            Shape c = new Circle();
+            c.Draw();
+
+            Shape r = new Rectangle();
+            r.Draw();
+
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
+
+    class Shape
+    {
+        // ---virtual---
+        //virtual keyword allows methods to be overrided in derived classes
+        public virtual void Draw()
+        {
+            Console.WriteLine("Base Draw");
+        }
+    }
+
+    class Circle : Shape
+    {
+        public override void Draw()
+        {
+            //draw a circle and stuff...
+            Console.WriteLine("Circle Drawn");
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            //draw a rectangle and stuff...
+            Console.WriteLine("Rectangle Drawn");
+        }
+    }
+
+    class Tesseract : Shape
+    {
+        public override void Draw()
+        {
+            //draw a... thing 
+            Console.WriteLine("Ma che cazz...!?!?!");
+        }
+    }
+
 }
+
